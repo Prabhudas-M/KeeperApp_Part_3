f7195c8aaaba09cc4b5009a71071ee85743d0e65
Prabhudas-M <60952658+Prabhudas-M@users.noreply.github.com>
2020-12-13 18:46:15 -0500

    a

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CreateArea() {
+  const [note, setNote] = useState({ title: "", content: "" });
   return (
     <div>
       <form>
